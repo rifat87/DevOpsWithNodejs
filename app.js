e0dbcc5b4fcec662464a0255db540acede8d9bd6
Rifat <tmrifat87@gmail.com>
2024-07-04 22:19:14 +0600

    Update2

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express")
+const mongoose = require( "mongoose");
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
 
 const app = express()
+
+mongoose.connect("mongodb://admin:password@mongo:27017/?authsource=admin").then(()=> console.log("Successfully connected to dB")).catch((e) => console.log("Error is: ", e));
 
 app.get("/", (req, res) => {
     res.send("<h2> Hi....., I am Tahzib, wait there i am  comming </h2>")
